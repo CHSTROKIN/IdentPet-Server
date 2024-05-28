55363ed883f0b80a3ff8e4e823e31a7061fb43b5
Owen Li <owen.li22@imperial.ac.uk>
2024-05-28 21:42:32 +0100

    feat: added /sighting API endpoint and /image endpoint for testing.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, make_response
 import model
 
 app = Flask(__name__)
+
+DUMMY_DATA = []
 
 @app.route("/", methods=["GET"])
 def index():
@@ -16,3 +18,20 @@ def embed():
         model.CONFIG["device"]
     )
     return {"embedding": embedding.tolist()}
+
+# Dummy /image endpoint for testing.
+@app.route("/image", methods=["POST"])
+def image():
+    return make_response(jsonify({"id": "dummy-id"}), 200)
+
+# Dummy /sighting endpoint for testing.
+@app.route("/sighting", methods=["POST"])
+def sighting():
+    data = request.json
+    DUMMY_DATA.append(data)
+    return make_response(jsonify({}), 200)
+
+# Dummy /debug/sighting route for debugging.
+@app.route("/debug/sighting", methods=["GET"])
+def sighting_debug():
+    return str(DUMMY_DATA)
