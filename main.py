54a8a38cc802a515a755398a2457375b58e840f4
Owen Li <owen.li22@imperial.ac.uk>
2024-05-29 21:41:45 +0100

    feat: added API endpoints to allow the association of an image with a pet.

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ import model
 app = Flask(__name__)
 db = firestore.Client(project="petfinder-424117")
 
-DUMMY_DATA = []
-
 @app.route("/", methods=["GET"])
 def index():
     return "Hello, World!"
@@ -35,7 +33,7 @@ def image():
     storage_client = storage.Client()
     bucket = storage_client.bucket(model.CONFIG["bucket_name"])
     
-    fname = "sighting_" + str(uuid.uuid4()) + ".jpg"
+    fname = "image_" + str(uuid.uuid4()) + ".jpg"
     blob = bucket.blob(fname)
 
     blob.upload_from_string(
@@ -58,3 +56,58 @@ def sighting():
     db.collection("sightings").add(data)
     
     return make_response(jsonify({}), 200)
+
+@app.route("/pet", methods=["GET"])
+def pet():
+    pets = db.collection("pets").stream()
+    pet_data = []
+    for pet in pets:
+        images = pet.get("images")
+        summary_image = images[0]
+        
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(model.CONFIG["bucket_name"])
+        blob = bucket.blob(summary_image)
+        blob.make_public()
+        
+        pet_data.append({
+            "id": pet.id,
+            "image": blob.public_url 
+        })
+    
+    return make_response(jsonify(pet_data), 200)
+
+@app.route("/pet/images", methods=["GET", "POST"])
+def pet_images():
+    if request.method == "GET":
+        images = db.collection("pets").document(request.args.get("id")).get().to_dict()["images"]
+        image_urls = []
+        
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(model.CONFIG["bucket_name"])
+    
+        for fname in images:
+            blob = bucket.blob(fname)
+            blob.make_public()
+            image_urls.append(blob.public_url)
+            
+        return make_response(jsonify(image_urls), 200)
+    else:
+        data = request.json
+        if data["mode"] == "add":
+            data["id"] = str(uuid.uuid4())
+        
+        ref = db.collection("pets").document(data["id"]).get()
+        
+        if ref.exists:
+            db.collection("pets").document(data["id"]).update({
+                "images": firestore.ArrayUnion([data["imageID"]])
+            })
+        else:
+            db.collection("pets").document(data["id"]).set({
+                "images": [data["imageID"]]
+            })
+        
+        return make_response(jsonify({
+            "petID": data["id"]
+        }), 200)
