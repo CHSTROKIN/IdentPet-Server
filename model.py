7575a5b5b324262ae2fb29dcb6c4b0cfcb8bc7e0
Owen Li <owen.li22@imperial.ac.uk>
2024-06-12 13:15:24 +0100

    fix: model will no longer fail due to circular imports.

--- model.py
+++ model.py
@@ -2,15 +2,10 @@ import vertexai # type: ignore
 from vertexai.vision_models import Image, MultiModalEmbeddingModel # type: ignore
 from google.cloud.firestore_v1.vector import Vector # type: ignore
 
-from database import DBInterface, CloudFilePath, ImageURL
-
 vertexai.init(project="petfinder-424117")
 model = MultiModalEmbeddingModel.from_pretrained("multimodalembedding")
 
-def embed_image_from_url(url: ImageURL) -> Vector:
+def embed_image_from_url(url: str) -> Vector:
     image = Image.load_from_file(url)
     embeddings = model.get_embeddings(image=image, dimension=512)
     return Vector(embeddings.image_embedding)
-
-def embed_image_from_fname(dbi: DBInterface, fname: CloudFilePath) -> Vector:
-    return embed_image_from_url(dbi.publish_image(fname))
