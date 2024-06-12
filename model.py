8eaf23fe4bd18b9b3c2d4d7da3b1a57d12eb5abf
Owen Li <owen.li22@imperial.ac.uk>
2024-06-12 13:12:18 +0100

    fix: model will now correct return vector type embeddings.

--- model.py
+++ model.py
@@ -1,15 +1,16 @@
 import vertexai # type: ignore
 from vertexai.vision_models import Image, MultiModalEmbeddingModel # type: ignore
+from google.cloud.firestore_v1.vector import Vector # type: ignore
 
 from database import DBInterface, CloudFilePath, ImageURL
 
 vertexai.init(project="petfinder-424117")
 model = MultiModalEmbeddingModel.from_pretrained("multimodalembedding")
 
-def embed_image_from_url(url: ImageURL):
+def embed_image_from_url(url: ImageURL) -> Vector:
     image = Image.load_from_file(url)
     embeddings = model.get_embeddings(image=image, dimension=512)
-    return embeddings.image_embedding
+    return Vector(embeddings.image_embedding)
 
-def embed_image_from_fname(dbi: DBInterface, fname: CloudFilePath):
+def embed_image_from_fname(dbi: DBInterface, fname: CloudFilePath) -> Vector:
     return embed_image_from_url(dbi.publish_image(fname))
